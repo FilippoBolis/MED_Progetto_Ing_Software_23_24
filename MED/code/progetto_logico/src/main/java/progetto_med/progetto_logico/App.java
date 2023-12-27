6af8d3f6547577f155c409dbba8f3fe43cf790d4
DanieleGotti <145766703+DanieleGotti@users.noreply.github.com>
2023-12-27 11:14:23 +0100

    Creazione progetti

---
+++ MED/code/progetto_logico/src/main/java/progetto_med/progetto_logico/App.java
@@ -0,0 +1,13 @@
+package progetto_med.progetto_logico;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
