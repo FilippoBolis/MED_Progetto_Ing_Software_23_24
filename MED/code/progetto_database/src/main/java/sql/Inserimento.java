86ce5b86c1ec4ca99bbf3ab64cc6194849b7875b
FilippoBolis <145765573+FilippoBolis@users.noreply.github.com>
2023-12-29 13:13:08 +0100

    Completamento codice per inserimento
    Co-Authored-By: DanieleGotti <145766703+DanieleGotti@users.noreply.github.com>
    Co-Authored-By: Gabriele-Mazzoleni <146365387+Gabriele-Mazzoleni@users.noreply.github.com>
    Co-Authored-By: GabrieleMasinari27 <153751660+GabrieleMasinari27@users.noreply.github.com>

--- MED/code/progetto_database/src/main/java/sql/Inserimento.java
+++ MED/code/progetto_database/src/main/java/sql/Inserimento.java
@@ -11,7 +11,7 @@ public class Inserimento {
 			if (conn != null) {
 				if (mansione == "M" || mansione == "I" || mansione == "S") {
 					Statement stmt = conn.createStatement();
-					String sql = "INSERT INTO PERSONALE(CODICE,NOME,COGNOME,MANSIONE) VALUES ('" + codice + "','" + nome + "','" + cognome + "','" + mansione + "')";
+					String sql = "INSERT INTO PERSONALE (CODICE,NOME,COGNOME,MANSIONE) VALUES ('" + codice + "','" + nome + "','" + cognome + "','" + mansione + "')";
 					stmt.executeUpdate(sql);
 					stmt.close();
 					conn.close();
@@ -21,7 +21,6 @@ public class Inserimento {
 				}
 
 			}
-			else {System.out.println("Non va");}
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
@@ -33,7 +32,7 @@ public class Inserimento {
 			if (conn != null) {
 				if (urgenza >= 0 && urgenza <= 2) {
 					Statement stmt = conn.createStatement();
-					String sql = "INSERT INTO DEGENTE VALUES (" + codice + "," + nome + "," + cognome + "," + urgenza + "," + attesa + ")";
+					String sql = "INSERT INTO DEGENTE (CODICE,NOME,COGNOME,URGENZA,IN_ATTESA) VALUES ('" + codice + "','" + nome + "','" + cognome + "','" + urgenza + "','" + attesa + "')";
 					stmt.executeUpdate(sql);
 					stmt.close();
 					conn.close();
@@ -48,21 +47,21 @@ public class Inserimento {
 		}
 	}
 
-	public void rilevazione(String ID, String codDeg, double temp, int pressMax,int pressMin, int glicem,LocalDate data, LocalTime ora, int freqCard, int dol) {try {
-		
-		Connection conn = DriverManager.getConnection(CreateDB.DB_URL);
-		if (conn != null) {
-			Statement stmt = conn.createStatement();
-			String sql = "INSERT INTO RILEVAZIONE(ID,CODICE_DEGENTE,TEMPERATURA,PRESSIONE_MAX,PRESSIONE_MIN,GLICEMIA,DATA,ORA,FREQ_CARD,DOLORE) VALUES ('" + ID + "','" + codDeg +"','" + temp + "','" + pressMax + "','" + pressMin + "','" + glicem+ "','" + data + "','" + ora+ "','" + freqCard+ "','" + dol + "')";
-			stmt.executeUpdate(sql);
-			stmt.close();
-			conn.close();
-			System.out.println("Rilevazione inserita con successo");
-
-		}
-	} catch (SQLException e) {
+	public void rilevazione(String ID, String codDeg, double temp, int pressMax,int pressMin, int glicem,LocalDate data, LocalTime ora, int freqCard, int dol) {
+			try {
+			Connection conn = DriverManager.getConnection(CreateDB.DB_URL);
+			if (conn != null) {
+				Statement stmt = conn.createStatement();
+				String sql = "INSERT INTO RILEVAZIONE (ID,CODICE_DEGENTE,TEMPERATURA,PRESSIONE_MAX,PRESSIONE_MIN,GLICEMIA,DATA,ORA,FREQ_CARD,DOLORE) VALUES ('" + ID + "','" + codDeg +"','" + temp + "','" + pressMax + "','" + pressMin + "','" + glicem+ "','" + data + "','" + ora+ "','" + freqCard+ "','" + dol + "')";
+				stmt.executeUpdate(sql);
+				stmt.close();
+				conn.close();
+				System.out.println("Rilevazione inserita con successo");
+	
+			}
+		} catch (SQLException e) {
 		System.out.println(e.getMessage());
-	}
+		}
 	}
 
 	public void reparto(String codice, String nome) {
@@ -70,7 +69,7 @@ public class Inserimento {
 			Connection conn = DriverManager.getConnection(CreateDB.DB_URL);
 			if (conn != null) {
 				Statement stmt = conn.createStatement();
-				String sql = "INSERT INTO REPARTO VALUES (" + codice + "," + nome + ")";
+				String sql = "INSERT INTO REPARTO (CODICE,NOME_REPARTO) VALUES ('" + codice + "','" + nome + "')";
 				stmt.executeUpdate(sql);
 				stmt.close();
 				conn.close();
@@ -87,7 +86,7 @@ public class Inserimento {
 			Connection conn = DriverManager.getConnection(CreateDB.DB_URL);
 			if (conn != null) {
 				Statement stmt = conn.createStatement();
-				String sql = "INSERT INTO MODULO VALUES (" + codRep + "," + nome + ")";
+				String sql = "INSERT INTO MODULO (CODICE_REPARTO,NOME) VALUES ('" + codRep + "','" + nome + "')";
 				stmt.executeUpdate(sql);
 				stmt.close();
 				conn.close();
@@ -104,7 +103,7 @@ public class Inserimento {
 			Connection conn = DriverManager.getConnection(CreateDB.DB_URL);
 			if (conn != null) {
 				Statement stmt = conn.createStatement();
-				String sql = "INSERT INTO LETTO VALUES (" + codRep + "," + nomeMod + "," + numero + ")";
+				String sql = "INSERT INTO LETTO (CODICE_REPARTO,NOME_MODULO,NUMERO) VALUES ('" + codRep + "','" + nomeMod + "','" + numero + "')";
 				stmt.executeUpdate(sql);
 				stmt.close();
 				conn.close();
@@ -116,12 +115,12 @@ public class Inserimento {
 		}	
 	}
 
-	public void assegnazioneLetto(String codDeg, String codRep, String nomeMod, int numero, Date dataAss) {
+	public void assegnazioneLetto(String codDeg, String codRep, String nomeMod, int numero, LocalDate dataAss) {
 		try {
 			Connection conn = DriverManager.getConnection(CreateDB.DB_URL);
 			if (conn != null) {
 				Statement stmt = conn.createStatement();
-				String sql = "INSERT INTO ASSEGNAZIONELETTO VALUES (" + codDeg+ "," + codRep + "," + nomeMod + "," + numero + "," + dataAss+ ")";
+				String sql = "INSERT INTO ASSEGNAZIONELETTO (CODICE_DEGENTE,CODICE_REPARTO,NOME_MODULO,NUMERO_LETTO,DATA_ASSEGNAZIONE) VALUES ('" + codDeg+ "','" + codRep + "','" + nomeMod + "','" + numero + "','" + dataAss + "')";
 				stmt.executeUpdate(sql);
 				stmt.close();
 				conn.close();
@@ -133,12 +132,12 @@ public class Inserimento {
 		}	
 	}
 
-	public void diariaInf(String codice, String codiceDegente, String codiceInfermiere, Date data, Time ora, String noteParticolari, Boolean importante, String farmaco) {
+	public void diariaInf(String codice, String codiceDegente, String codiceInfermiere, LocalDate data, LocalTime ora, String noteParticolari, Boolean importante, String farmaco) {
 		try {
 			Connection conn = DriverManager.getConnection(CreateDB.DB_URL);
 			if (conn != null) {
 				Statement stmt = conn.createStatement();
-				String sql = "INSERT INTO DIARIAINF VALUES (" + codice + "," + codiceDegente + "," + codiceInfermiere + "," + data + "," + ora + "," + noteParticolari + "," + importante + "," + farmaco + ")";
+				String sql = "INSERT INTO DIARIAINF (CODICE,CODICE_DEGENTE,CODICE_INFERMIERE,DATA,ORA,NOTE_PARTICOLARI,IMPORTANTE,FARMACO) VALUES ('" + codice + "','" + codiceDegente + "','" + codiceInfermiere + "','" + data + "','" + ora + "','" + noteParticolari + "','" + importante + "','" + farmaco + "')";
 				stmt.executeUpdate(sql);
 				stmt.close();
 				conn.close();
@@ -149,12 +148,12 @@ public class Inserimento {
 		}
 	}
 
-	public void diariaMed(String codice, String codiceDegente, String codiceMedico, String storico, String motivo, String farmaci, Date data, String ora, String allergie) {
+	public void diariaMed(String codice, String codiceDegente, String codiceMedico, String storico, String motivo, String farmaci, LocalDate data, LocalTime ora, String allergie) {
 		try {
 			Connection conn = DriverManager.getConnection(CreateDB.DB_URL);
 			if (conn != null) {
 				Statement stmt = conn.createStatement();
-				String sql = "INSERT INTO DIARIAINF VALUES (" + codice + "," + codiceDegente + "," + codiceMedico + "," + storico + "," + motivo + "," + farmaci + "," + data + "," + ora + "," + allergie + ")";
+				String sql = "INSERT INTO DIARIAMED (CODICE,CODICE_DEGENTE,CODICE_MEDICO,STORICO,MOTIVO,FARMACI,DATA,ORA,ALLERGIE) VALUES ('" + codice + "','" + codiceDegente + "','" + codiceMedico + "','" + storico + "','" + motivo + "','" + farmaci + "','" + data + "','" + ora + "','" + allergie + "')";
 				stmt.executeUpdate(sql);
 				stmt.close();
 				conn.close();
@@ -171,15 +170,16 @@ public class Inserimento {
 
 	public static void main(String[] args) {
             Inserimento i= new Inserimento();
-            i.personale("A1","Daniele","Gotti","M");
-          //  i.degente("A21","Gabriele","Mazzoleni",0,true);
-            i.rilevazione("B1","A21",36.8,150,90,100, LocalDate.now(),LocalTime.now().withNano(0),60,5);
-           /* i.reparto();
-            i.modulo();
-            i.letto();
-            i.assegnazioneLetto();
-            i.diariaInf();
-            i.diariaMed();*/
+            i.personale("P1","Daniele","Gotti","M");
+            i.personale("P2","Filippo","Bolis","I");
+            i.degente("D1","Gabriele","Mazzoleni",0,true);
+            i.rilevazione("Ri1","D1",36.8,150,90,100, LocalDate.now(),LocalTime.now().withNano(0),60,5);
+            i.reparto("Re1","Cardiologia");
+            i.modulo("Re1","ModuloA");
+            i.letto("Re1","ModuloA",1);
+            i.assegnazioneLetto("D1","Re1","ModuloA",1,LocalDate.now());
+            i.diariaInf("DiariaInf1","D1","P2",LocalDate.now(),LocalTime.now().withNano(0),"Il paziente è diventato enorme dottore",true,"Trembolone");
+            i.diariaMed("DiariaMed1","D1","P1","il paziente non soffre di nulla in particolare","Vuole diventare più grosso","Trembolone",LocalDate.now(),LocalTime.now().withNano(0),"nessun allergia");
             
             
             
