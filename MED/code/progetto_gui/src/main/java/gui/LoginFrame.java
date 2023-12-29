d73c2d02f0093ff98f0cd5541991d3373eb9d82d
DanieleGotti <145766703+DanieleGotti@users.noreply.github.com>
2023-12-29 19:57:50 +0100

    Login gui
    Co-Authored-By: FilippoBolis <145765573+FilippoBolis@users.noreply.github.com>
    Co-Authored-By: Gabriele-Mazzoleni <146365387+Gabriele-Mazzoleni@users.noreply.github.com>
    Co-Authored-By: GabrieleMasinari27 <153751660+GabrieleMasinari27@users.noreply.github.com>

--- MED/code/progetto_gui/src/main/java/gui/LoginFrame.java
+++ MED/code/progetto_gui/src/main/java/gui/LoginFrame.java
@@ -1,13 +1,41 @@
 package gui;
 
-public class LoginFrame {
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.formdev.flatlaf.FlatIntelliJLaf;
+
+public class LoginFrame extends JFrame {
 
 	public LoginFrame() {
 		
+	 	try {
+			UIManager.setLookAndFeel(new FlatIntelliJLaf());
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+	 	
+	 	JFrame loginFrame = new JFrame();
+	 	loginFrame.setSize(900, 500);
+	 	loginFrame.setTitle("Login");
+	 	loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 	loginFrame.setLocationRelativeTo(null);
+	 	loginFrame.setResizable(false);
+	 	
+	 	JPanel loginPanel = new JPanel();
+	 	loginPanel.setBackground(new Color(192, 192, 192));
+        loginFrame.getContentPane().add(loginPanel);
+        loginPanel.setLayout(null);
+        
+        loginFrame.setVisible(true);
 	}
 	
 	public static void main(String[] args) throws Exception {
-		
+		LoginFrame f = new LoginFrame();
 		
 	}
 	
