994cdaf72e42b7bdf099447d46f9f63a7c8398ff
FilippoBolis <145765573+FilippoBolis@users.noreply.github.com>
2024-01-03 14:13:50 +0100

    Eliminazione della logica di prima
    Co-Authored-By: DanieleGotti <145766703+DanieleGotti@users.noreply.github.com>
    Co-Authored-By: Gabriele-Mazzoleni <146365387+Gabriele-Mazzoleni@users.noreply.github.com>
    Co-Authored-By: GabrieleMasinari27 <153751660+GabrieleMasinari27@users.noreply.github.com>

--- MED/code/progetto_logico/src/main/java/logiche_bottoni_login/loginLogic.java
+++ MED/code/progetto_logico/src/main/java/logiche_bottoni_login/loginLogic.java
@@ -1,8 +1,5 @@
 package logiche_bottoni_login;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
@@ -22,7 +19,6 @@ public class loginLogic {
 	LoginFrame view;
 	public static String DB_REL_FILELOGIC = "../progetto_database/db/db.db3";
 	public static String DB_URLLOGIC = "jdbc:sqlite:" + DB_REL_FILELOGIC;
-    private static Lock lock = new ReentrantLock();
 	
 	public loginLogic(LoginFrame v) {
 		// prede i refs
@@ -31,42 +27,29 @@ public class loginLogic {
 	}
 	
 	private void registerLogin() {
-		
 		// si registra al bottone loginButton
 		view.loginButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (lock.tryLock()) {
 					try {
-						lock.lock();
-						logica();
+						Connection conn = DriverManager.getConnection(DB_URLLOGIC);
+						if (conn != null) {
+							DSLContext contesto = DSL.using(conn, SQLDialect.SQLITE);
+							String utente = view.userField.getText();
+							String password = new String(view.passwordField.getPassword());
+							int result = contesto.select().from(Personale.PERSONALE).where(Personale.PERSONALE.PASSWORD.eq(password).and(Personale.PERSONALE.CODICE.eq(utente))).execute();
+							if (result == 1) {
+								System.out.println("ok");
+								view.sfondoFrame.setVisible(false);
+							}
+							else {
+								System.out.println("non ok");
+							}
+						}
+					} catch (SQLException ev) {
+						System.out.println(ev.getMessage());
 					}
-					finally {
-						lock.unlock();
-					}
-				}
 			}
 		});
-	}
-	
-	private void logica() {
-		try {
-			Connection conn = DriverManager.getConnection(DB_URLLOGIC);
-			if (conn != null) {
-				DSLContext contesto = DSL.using(conn, SQLDialect.SQLITE);
-				String utente = view.userField.getText();
-				String password = new String(view.passwordField.getPassword());
-				int result = contesto.select().from(Personale.PERSONALE).where(Personale.PERSONALE.PASSWORD.eq(password).and(Personale.PERSONALE.CODICE.eq(utente))).execute();
-				if (result == 1) {
-					System.out.println("ok");
-					view.sfondoFrame.setVisible(false);
-				}
-				else {
-					System.out.println("non ok");
-				}
-			}
-		} catch (SQLException ev) {
-			System.out.println(ev.getMessage());
-		}
 	}
 }
