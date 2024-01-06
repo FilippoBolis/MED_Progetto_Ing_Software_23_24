4cefbda4fce1a85982dabe540b8b5a05d0b95679
DanieleGotti <145766703+DanieleGotti@users.noreply.github.com>
2024-01-06 02:04:38 +0100

    Frame Principale

--- MED/code/progetto_gui/src/main/java/gui/PazientiFrame.java
+++ MED/code/progetto_gui/src/main/java/gui/PazientiFrame.java
@@ -12,18 +12,24 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
+import javax.swing.table.TableRowSorter;
 
 import com.formdev.flatlaf.FlatIntelliJLaf;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.BorderLayout;
 
 public class PazientiFrame {
 	
-	public JFrame sfondoFrame;
-	public DefaultTableModel tableModel;
-	public JTable table;
-	
 	private final int WIDTH = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
 	private final int HEIGHT = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
-	
+	public JFrame sfondoFrame;
+	public String infoPaziente = "Seleziona un paziente nel database";
+	public String mansioneUtente = "Dott.";
+	public String cognomeUtente = "Gotti";
+	public DefaultTableModel tableModel;
+	public JTable table;
+
 	@SuppressWarnings("serial")
 	public PazientiFrame() {
 		
@@ -39,7 +45,7 @@ public class PazientiFrame {
 		sfondoFrame.getRootPane().setForeground(Color.WHITE);
 		sfondoFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		sfondoFrame.setLocationRelativeTo(null);
-		sfondoFrame.setResizable(true);
+		sfondoFrame.setResizable(false);
 		sfondoFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		
 		JPanel sfondoPanel = new JPanel();
@@ -47,16 +53,61 @@ public class PazientiFrame {
 		sfondoFrame.getContentPane().add(sfondoPanel);
 		
         JPanel infoPanel = new JPanel();
-        infoPanel.setBackground(Color.RED);
-        infoPanel.setBounds((int) (WIDTH * 0.25), 0, WIDTH, (int)(HEIGHT * 0.25));
+        infoPanel.setBounds((int) (WIDTH * 0.25), 0, (int) (WIDTH * 0.75), (int)(HEIGHT * 0.25));
         infoPanel.setLayout(null);
         sfondoPanel.add(infoPanel);
-
+        
+        JLabel utenteLabel = new JLabel("Utente: " + mansioneUtente + " " + cognomeUtente);
+        utenteLabel.setBounds(20, 0, (int) (WIDTH * 0.75) - 40, (int) (infoPanel.getHeight() * 0.25));
+        utenteLabel.setForeground(Stile.BLU_SCURO.getColore());
+        utenteLabel.setFont(Stile.TESTO.getFont());
+        infoPanel.add(utenteLabel);
+        
+        JPanel pazientePanel = new JPanel();
+        pazientePanel.setBounds(0, utenteLabel.getHeight(), (int) (WIDTH * 0.75), (int) (infoPanel.getHeight() * 0.75));
+        pazientePanel.setBackground(Stile.BLU.getColore());
+        infoPanel.add(pazientePanel);
+        pazientePanel.setLayout(null);
+        
+        JLabel pazienteTitoloLabel = new JLabel("Paziente");
+        pazienteTitoloLabel.setVerticalAlignment(SwingConstants.BOTTOM);
+        pazienteTitoloLabel.setBounds(20, 0, (int) (pazientePanel.getWidth() - 20), (int) (pazientePanel.getHeight() * 0.5));
+        pazienteTitoloLabel.setForeground(Color.WHITE);
+        pazienteTitoloLabel.setFont(Stile.TITOLO_FINE.getFont());
+        pazientePanel.add(pazienteTitoloLabel);
+        
+        JLabel pazienteLabel = new JLabel(infoPaziente);
+        pazienteLabel.setVerticalAlignment(SwingConstants.TOP);
+        pazienteLabel.setBounds(20, pazienteTitoloLabel.getHeight(), (int) (pazientePanel.getWidth() - 20), (int) (pazientePanel.getHeight() * 0.5));
+        pazienteLabel.setForeground(Color.WHITE);
+        pazienteLabel.setFont(Stile.SOTTOTITOLO_FINE.getFont());
+        pazientePanel.add(pazienteLabel);
+        
 		JPanel sinistraPanel = new JPanel();
 		sinistraPanel.setBounds(0, 0, (int) (WIDTH * 0.25), HEIGHT);
 		sinistraPanel.setBackground(Stile.BLU_SCURO.getColore());
 		sinistraPanel.setLayout(null);
 		sfondoPanel.add(sinistraPanel);
+		
+        JLabel titoloLabel = new JLabel("Medical Environment Database");
+        titoloLabel.setBounds(20, 0, WIDTH, (int) (infoPanel.getHeight() * 0.25));
+        titoloLabel.setForeground(Color.WHITE);
+        titoloLabel.setFont(Stile.SOTTOTITOLO.getFont());
+        //titoloLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        sinistraPanel.add(titoloLabel);
+        		
+        JPanel logoPanel = new JPanel();
+        logoPanel.setBounds(0, utenteLabel.getHeight(), (int) (WIDTH * 0.75), (int) (infoPanel.getHeight() * 0.75));
+        logoPanel.setBackground(Stile.BLU_SCURO.getColore());
+        logoPanel.setLayout(new BorderLayout(0, 0));
+        sinistraPanel.add(logoPanel);
+        
+        JLabel logoLabel = new JLabel("logo");
+        logoLabel.setBounds(0, 0, WIDTH, (int) (infoPanel.getHeight() * 0.25));
+        logoLabel.setForeground(Color.WHITE);
+        logoLabel.setFont(Stile.SOTTOTITOLO.getFont());
+        //logoLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        logoPanel.add(logoLabel);
 		
 		JPanel centroPanel = new JPanel();
 		centroPanel.setBounds((int) (WIDTH * 0.25), (int) (HEIGHT * 0.25), (int) (WIDTH * 0.75), (int) (HEIGHT * 0.75));
@@ -64,11 +115,13 @@ public class PazientiFrame {
 		sfondoPanel.add(centroPanel);
 		
         tableModel = new DefaultTableModel();
-        tableModel.addColumn("CODICE");
-        tableModel.addColumn("NOME");
-        tableModel.addColumn("COGNOME");
-        tableModel.addColumn("URGENZA");
-        tableModel.addColumn("IN ATTESA");
+        tableModel.addColumn("Codice");
+        tableModel.addColumn("Cognome");
+        tableModel.addColumn("Nome");
+        tableModel.addColumn("Sesso");
+        tableModel.addColumn("Data");
+        tableModel.addColumn("Ora");
+        tableModel.addColumn("Urgenza");
         
         table = new JTable(tableModel) {
             @Override
@@ -76,37 +129,55 @@ public class PazientiFrame {
                 return false;
             }
         };
-        table.setRowHeight(40);
+        table.setRowHeight(30);
         table.setShowHorizontalLines(true);
         table.setShowVerticalLines(true);
         table.setSelectionBackground(Stile.AZZURRO_TRASP.getColore());
         
+        //TOGLIERE solo di prova
+        tableModel.addRow(new Object[]{"1", "2", "1", "2", "1", "2", "1"});
+        tableModel.addRow(new Object[]{"2", "1", "2", "1", "2", "1", "2"});
+        for (int i = 0; i < 30; i++) {
+            tableModel.addRow(new Object[]{"3", "3", "3", "3", "3", "3", "3"});
+        }
+        
 		JScrollPane scrollPanel = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-        scrollPanel.setBounds(20, 20, (int) (WIDTH * 0.75) - 40, (int) (HEIGHT * 0.75) - 90 );
+        scrollPanel.setBounds(20, 20, (int) (WIDTH * 0.75) - 40, (int) (HEIGHT * 0.75) - 60);
         scrollPanel.setBorder(BorderFactory.createEmptyBorder()); 
         scrollPanel.setViewportView(table);
         centroPanel.add(scrollPanel);
         
         JTableHeader tableHeader = table.getTableHeader();
-        tableHeader.setPreferredSize(new Dimension(table.getColumnModel().getTotalColumnWidth(), 40));
+        tableHeader.setPreferredSize(new Dimension(table.getColumnModel().getTotalColumnWidth(), 30));
         tableHeader.setBackground(Stile.AZZURRO.getColore());
         tableHeader.setForeground(Color.WHITE);
-        tableHeader.setFont(Stile.SOTTOTITOLO.getFont());
-        tableHeader.setEnabled(false);
+        tableHeader.setFont(Stile.TESTO.getFont());
         
-        
-
-        
-        //TOGLIERE
-        for (int i = 0; i < 30; i++) {
-            tableModel.addRow(new Object[]{"a", "a", "a", "a", "a"});
-        }
-       
-        
-		
+        //FILIPPO se clicchi sul nome di una qualsiasi colonna le ordina crescenti, se riclicchi decrescenti. Può bastare come filtro? 
+        TableRowSorter<DefaultTableModel> ordineColonna = new TableRowSorter<>(tableModel);
+        table.setRowSorter(ordineColonna);
 		
 		sfondoFrame.setVisible(true);
 	}
+	
+	public void updateStringaUtente() {
+		//FILIPPO
+		//Crea e collega il modello a questa funzione come nell'esempio calculator
+		//All'avvio del frame la variabile mansioneUtente (se M metti Dott., se I metti Inf.) e cognomeUtente vanno inserite correttamente in base al Login
+		//(ho messo le stringhe dichiarate con valori a caso in alto, poi toglile quando hai fatto)
+		//Appariranno nel label utenteLabel
+	}
+	
+	public void updateStringaPaziente() {
+		//FILIPPO
+		//Crea e collega il modello a questa funzione come nell'esempio calculator
+		//Se l'utente non ha neancora cliccato sul paziente viene visualizzata la frase: "Selezionare un paziente nel database" nel label pazienteLabel
+		//(ho messo la stringa dichiarata con valori a caso in alto, poi toglila quando hai fatto)
+		//Se l'utente clicca sulla tabella viene visualizzata una stringa con tutti i dati: "Codice: 3123 Cognome: Gotti ecc..."
+	}
+	
+	//FILIPPO 
+	//Sotto al logo che pulsanti metto?
 	
 	public static void main(String[] args) throws Exception {
 		new PazientiFrame();
--- MED/code/progetto_gui/src/main/java/gui/Stile.java
+++ MED/code/progetto_gui/src/main/java/gui/Stile.java
@@ -8,13 +8,16 @@ public enum Stile {
     BLU(new Color(26, 69, 120)),
     BLU_SCURO(new Color(17, 45, 78)),
     AZZURRO(new Color(63, 114, 175)),
-	AZZURRO_TRASP(new Color(63, 114, 175, 100)),
+	AZZURRO_TRASP(new Color(63, 114, 175, 50)),
 	VERDE(new Color(60, 179, 113)),
 
 	SPECIALE(new Font("sanserif", Font.BOLD, 40)),
 	TITOLO(new Font("sanserif", Font.BOLD, 30)),
+	TITOLO_FINE(new Font("sanserif", Font.PLAIN, 30)),
     SOTTOTITOLO(new Font("sanserif", Font.BOLD, 16)),
+    SOTTOTITOLO_FINE(new Font("sanserif", Font.PLAIN, 16)),
     TESTO(new Font("sanserif", Font.BOLD, 12)),
+    TESTO_FINE(new Font("sanserif", Font.PLAIN, 12)),
 	CORSIVO(new Font("sanserif", Font.ITALIC, 12));
 
     private final Color colore;
